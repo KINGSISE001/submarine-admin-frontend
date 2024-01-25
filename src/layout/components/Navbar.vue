bf7b9d3fdbbf44246f4ed5e5ee6cc8e969363447
RISE <949092641@qq.com>
2024-01-25 10:12:55 +0800

    1

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -68,7 +68,7 @@ export default {
   height: 50px;
   overflow: hidden;
   position: relative;
-  background: #fff;
+  background: #EEEEEE;
   box-shadow: 0 1px 4px rgba(0,21,41,.08);
 
   .hamburger-container {
